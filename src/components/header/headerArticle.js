993b7275972ce1a0accb367af149eb5c88c9b3a1
nephiejoy <stephanie@falkberg.dk>
2022-10-12 11:06:16 +0200

    extract css

--- src/components/header/headerArticle.js
+++ src/components/header/headerArticle.js
@@ -1,16 +1,19 @@
 
-let article = function(hero) {
+let headerArticle = function(hero) {
 
 let element = document.createElement('article')
 element.classList.add('headerArticle')
 
 element.innerHTML = `
-    <h1>${hero.headline}</h1>
-    <p>${hero.copy}</p>
-    <a href="${hero.icon}">Explore</a>
+    <h1 class="headerArticle__headline">${hero.headline}</h1>
+    <p class="headerArticle__text">${hero.copy}</p>
+    <a class="headerArticle__exploreButton" href="#">Explore</a>
+    <!-- <button><img class="headerArticle__icon" src="${hero.icon}" alt="">Explore</button> -->
+    <!-- <a class="headerArticle__icon" href="">${hero.icon} Explore</a> -->
+    <!-- <img src="${hero.icon}" alt=""> -->
 `
 
 return element
 }
 
-export default article
+export default headerArticle
---
+++ src/components/servicesSection/services.js
@@ -0,0 +1,13 @@
+let services = function() {
+
+let element = document.createElement('section')
+element.classList.add('services')
+
+element.innerHTML = `
+    
+`
+
+return element
+}
+
+export default services
